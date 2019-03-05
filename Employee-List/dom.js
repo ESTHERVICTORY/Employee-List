d513fec1a33c74b2c4644153d02ad840b72e80c3
ESTHERVICTORY <46943242+ESTHERVICTORY@users.noreply.github.com>
2019-03-05 00:45:35 -0500

    Update dom.js

--- Employee-List/dom.js
+++ Employee-List/dom.js
@@ -10,49 +10,49 @@
 //     hide:hide,
 //     show:show,
 //   };
-const prepend = function(content){
-    for(let i=0; i<nodeList.length;i++){
-        nodeList[i].innerHTML = content +nodeList[i].HTML;
-    }
-}
+// const prepend = function(content){
+//     for(let i=0; i<nodeList.length;i++){
+//         nodeList[i].innerHTML = content +nodeList[i].HTML;
+//     }
+// }
 
-const on = function(action,cb){
-    for (let i=0; i<nodeList.length;i++){
-       nodeList[i].addEventListener(action,cb);
-    }
-}
+// const on = function(action,cb){
+//     for (let i=0; i<nodeList.length;i++){
+//        nodeList[i].addEventListener(action,cb);
+//     }
+// }
 
-const val = function(content){
-    if(content === undefined){
-       return nodeList[0].value;
-    }else{
-        nodeList[0].value = content;
-    }
-}
-const show =function(){
-    for(let i=0; i< nodeList.length;i++){
-        nodeList[i].style.display =' ';
-    }
-}
+// const val = function(content){
+//     if(content === undefined){
+//        return nodeList[0].value;
+//     }else{
+//         nodeList[0].value = content;
+//     }
+// }
+// const show =function(){
+//     for(let i=0; i< nodeList.length;i++){
+//         nodeList[i].style.display =' ';
+//     }
+// }
 
-const hide =function(){
-    for(let i=0; i<nodeList.length;i++){
-        nodeList[i].style.display ='none';
-    }
-}
+// const hide =function(){
+//     for(let i=0; i<nodeList.length;i++){
+//         nodeList[i].style.display ='none';
+//     }
+// }
 
- return{
-     text:text,
-     html:html,
-     addClass:addClass,
-     removeClass: removeClass,
-     toggleClass: toggleClass,
-     empty:empty,
-     append: append, 
-     prepend: prepend,
-     on:on,
-     val:val,
-     show:show,
-     hide:hide,
-    //  showVerify:showVerify
- };
+//  return{
+//      text:text,
+//      html:html,
+//      addClass:addClass,
+//      removeClass: removeClass,
+//      toggleClass: toggleClass,
+//      empty:empty,
+//      append: append, 
+//      prepend: prepend,
+//      on:on,
+//      val:val,
+//      show:show,
+//      hide:hide,
+//     //  showVerify:showVerify
+//  };
